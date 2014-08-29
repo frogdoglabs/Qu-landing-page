83b3be0c16f88550014c0ba4448963f73dd790f3
ngthatsme <frogdoglabs@gmail.com>
2014-08-28 22:00:41 -0400

    inserting new gif on front page, moving video to second page

--- assets/subscribe.php
+++ assets/subscribe.php
@@ -10,7 +10,7 @@ function isEmail($email) {
 if($_POST) {
 
     // Enter the email where you want to receive the notification when someone subscribes
-    $emailTo = 'contact.frogdoglabs@gmail.com';
+    $emailTo = 'frogdoglabs@gmail.com';
 
     $subscriber_email = addslashes(trim($_POST['email']));
 
